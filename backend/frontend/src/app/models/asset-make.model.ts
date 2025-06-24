c9aecd54a30558496518f8021bdd04a8108a11a4
mahathi-celcom <nagamahathii@celcomsolutions.com>
2025-06-24 23:54:48 +0530

    Adding backend

--- backend/frontend/src/app/models/asset-make.model.ts
+++
@@ -1,6 +0,0 @@
-export interface AssetMake {
-  id?: number;
-  name: string;
-  typeId: number;
-  status?: string;
-} 
--- backend/frontend/src/app/models/asset-type.model.ts
+++
@@ -1,8 +0,0 @@
-export interface AssetType {
-  id?: number;
-  name: string;
-  description?: string;
-  status?: string;
-  assetCategory?: 'HARDWARE' | 'SOFTWARE';
-  assetTypeName?: string; // For display purposes
-} 
